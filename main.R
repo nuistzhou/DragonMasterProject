38f3cbd17d8b8eb8f5dfde135e67dddde93272a8
rodrigoalmeida94 <rodrigo.almeida94@outlook.pt>
2017-01-23 13:36:44 +0100

    update

--- main.R
+++ main.R
@@ -1,13 +1,21 @@
 
-
-source('R/download.R')
 library(raster)
+library(tools)
 library(rgdal)
 library(gdalUtils)
 library(rworldmap)
 
 system('python Python/download.py')
 system('Bash/./modis_ndvi.sh')
+
+hazards <- list.files('data', pattern = 'haz_*', full.names = T)
+for (haz in hazards){
+  assign(basename(file_path_sans_ext(haz)),raster(haz))
+}
+hazards <- basename(file_path_sans_ext(hazards))
+
+
+
 
 # Handles the HDF files
 sds <- get_subdatasets(list.files('data'))
