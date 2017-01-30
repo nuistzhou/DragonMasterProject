82c2e5908ec7d7ed775d204a017105355bdf2fb3
rodrigoalmeida94 <rodrigo.almeida94@outlook.pt>
2017-01-30 11:29:24 +0100

    creates file ndvi_mean

--- R/ndvi_annual_mean.R
+++ R/ndvi_annual_mean.R
@@ -8,12 +8,5 @@ ndvi_annual_mean <- function(ndvi_data, ndvi_reliability) {
     ndvi_stack <- stack(ndvi_stack, ndvi[[i]])
     }
   # Do mean calculation to get annual NDVI stack
-  return (calc(ndvi_stack, fun = mean, na.rm = T))
+  return (calc(ndvi_stack, fun = mean, na.rm = T, filename = 'data/ndvi_mean.tif'))
 }
-
-
-#ndvi[[1]] <- overlay(x=ndvi[[1]],y=ndvi_reliability[[1]],fun=function(x,y) ifelse(y %in% c(0,2,4),NA,x))
-#ndvi_stack <- stack(ndvi_stack, ndvi[[1]])
-
-#ndvi[[2]] <- overlay(x=ndvi[[2]],y=ndvi_reliability[[2]],fun=function(x,y) ifelse(y %in% c(0,2,4),NA,x))
-#ndvi_stack <- stack(ndvi_stack, ndvi[[2]])
