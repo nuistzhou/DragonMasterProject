adb2a73fc6c3b32dd64e6ac46f0826daae08aafd
Ubuntu User <ubuntu@packer-ubuntu-16.04-server-one>
2017-01-30 17:49:50 +0100

    updates

--- R/hazards_sum.R
+++ R/hazards_sum.R
@@ -1,6 +1,5 @@
 # Calculate hazards index
 hazards_sum <- function(cyclone,drought,earthquake,flood,landslide,volcano) {
   return (overlay(cyclone,drought,earthquake,flood,landslide,volcano,fun=sum,na.rm=T))
+  
 }
-
-hazards_sum(r_haz_cyclone,r_haz_drought,r_haz_earthquake,r_haz_flood,r_haz_landslide,r_haz_volcano)
