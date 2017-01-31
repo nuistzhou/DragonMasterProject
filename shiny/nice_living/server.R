5330f8cde1770228a3d97df2bd54d4fb1ec79a65
Ubuntu User <ubuntu@packer-ubuntu-16.04-server-one>
2017-01-31 17:54:04 +0100

    updates

--- shiny/nice_living/server.R
+++ shiny/nice_living/server.R
@@ -11,12 +11,19 @@ library(shiny)
 library(leaflet)
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-
-  output$niceliving <- renderPlot({
-    index <- calc_index(ndvi_mean,r_gecon_ppp, r_haz_flood, r_annualpm25,input$ndvi_wei,
+  dataInput <- reactive(  
+  tif_files <- list.files('data', pattern = '*', full.names = T)
+  for (tif in tif_files){
+            assign(basename(file_path_sans_ext(tif)),raster(tif))
+            source('calc_index.R')}
+    )
+  })
+    output$niceliving <- renderPlot({
+    dataInput()
+    index <- calc_index(ndvi_mean_m,r_gecon_ppp_m,haz_comp_m, r_annualpm25_m,input$ndvi_wei,
                input$haz_wei,input$gecon_wei,input$pm25_wei)
-    m <- leaflet(data=index) %>% addTiles
+    m <- leaflet(data=index) %>% addTiles %>%
+      addLegend(pal=qpal,values=index$data,opacity=1)
     m
       })
-  
 })
