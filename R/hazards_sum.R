50e3802a43a8790951c7089e560dbc17e56fb1b2
Ubuntu User <ubuntu@packer-ubuntu-16.04-server-one>
2017-01-31 11:27:51 +0100

    added some comments

--- R/hazards_sum.R
+++ R/hazards_sum.R
@@ -1,10 +1,12 @@
 # Calculate hazards index
 hazards_sum <- function(cyclone,drought,earthquake,flood,landslide,volcano){
+  # Set NA value to 0 to make it both easier and reasonable to calculate and display
   cyclone[is.na(cyclone[])] <- 0
   drought[is.na(drought[])] <- 0
   earthquake[is.na(earthquake[])] <- 0
   flood[is.na(flood[])] <- 0
   landslide[is.na(landslide[])] <- 0
   volcano[is.na(volcano[])] <- 0
+  # Calculate the sum of 6 different hazards
   return (sum(cyclone,drought,earthquake,flood,landslide,volcano)
 }
