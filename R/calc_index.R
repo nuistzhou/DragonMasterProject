547e106d0de98dd1e96a555f0899d4c1270ed9c6
Ubuntu User <ubuntu@packer-ubuntu-16.04-server-one>
2017-01-31 11:24:19 +0100

    now working properly

--- R/calc_index.R
+++ R/calc_index.R
@@ -1,13 +1,4 @@
 # calculate INDEX
-index <- function(ndvi_mean,gecon,harzards,pm25,wei_ndvi_mean,wei_gecon,
-                  wei_harzards,wei_pm25){
-  overlay(ndvi_mean,gecon,harzards,pm25,wei_ndvi_mean,wei_gecon,
-                 wei_harzards,wei_pm25,fun=function(a,b,c,d,e,f,g,h){
-                   (a*e+b*f-c*g-d*h)
-                 })
+calc_index <- function(ndvi_mean,gecon,harzards,pm25,wei_ndvi_mean,wei_gecon,wei_harzards,wei_pm25){
+  return (sum((ndvi_mean*wei_ndvi_mean),(gecon*wei_gecon),(pm25*wei_pm25),(harzards*wei_harzards),rm.na=T))
 }
-
-index(ndvi_mean,r_gecon_ppp,r_haz_flood,r_annualpm25,1,2,3,4)
-
-a_masked <- mask(a,simpleWorld)
-plot(a_masked)
--- R/hazards_sum.R
+++ R/hazards_sum.R
@@ -1,9 +1,10 @@
 # Calculate hazards index
-hazards_sum <- function(cyclone,drought,earthquake,flood,landslide,volcano) {
-  return (overlay(cyclone,drought,earthquake,flood,landslide,volcano,fun=sum,na.rm=T))
-<<<<<<< HEAD
+hazards_sum <- function(cyclone,drought,earthquake,flood,landslide,volcano){
+  cyclone[is.na(cyclone[])] <- 0
+  drought[is.na(drought[])] <- 0
+  earthquake[is.na(earthquake[])] <- 0
+  flood[is.na(flood[])] <- 0
+  landslide[is.na(landslide[])] <- 0
+  volcano[is.na(volcano[])] <- 0
+  return (sum(cyclone,drought,earthquake,flood,landslide,volcano)
 }
-=======
-  
-}
->>>>>>> origin/master
