b94f5f1852455f5e3cf781fc23866167177598b7
Ubuntu User <ubuntu@packer-ubuntu-16.04-server-one>
2017-02-01 18:59:26 +0100

    changes

--- R/vis.R
+++ R/vis.R
@@ -3,7 +3,7 @@ col_pal <- brewer.pal(10, "RdBu")
 mymap <-  leaflet() %>%
       addTiles() %>%
      setView(lng = 5.66, lat = 51.96, zoom = 5) %>%
-     addRasterImage(raster('data/index10101010.tif'), colors = col_pal, opacity = 0.6,group="Same weights") %>%
+     #addRasterImage(raster('data/index10101010.tif'), colors = col_pal, opacity = 0.6,group="Same weights") %>%
     addRasterImage(raster('data/index10050505.tif'), colors = col_pal, opacity = 0.6,group="Greenest") %>%
      addRasterImage(raster('data/index05100505.tif'), colors = col_pal, opacity = 0.6,group="Richest") %>%
      addRasterImage(raster('data/index05051005.tif'), colors = col_pal, opacity = 0.6,group="Less hazards") %>%
