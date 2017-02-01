16853fbea1e424b2b768cc30cb4ae7462afe0c57
Ubuntu User <ubuntu@packer-ubuntu-16.04-server-one>
2017-02-01 16:57:56 +0100

    updates

--- R/vis.R
+++ R/vis.R
@@ -1,21 +1,20 @@
 
 library(htmlwidgets)
 library(leaflet)
-
+library(RColorBrewer)
+col_pal <- brewer.pal(10, "RdBu")
 mymap <-  leaflet() %>%
       addTiles() %>%
-#      urlTemplate = "//{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png",
- #     attribution = 'Maps by <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-#    ) %>%
-    #      setView(lng = 52.52, lat = 13.40, zoom = 4) %>%
-     addRasterImage(raster('data/0.5_0.5_0.5_1.tif'), colors = rainbow(10), opacity = 0.8,group="Hazards Free") %>%
-     addRasterImage(raster('data/1_0.5_0.5_0.5.tif'), colors = rainbow(10), opacity = 0.8,group="Greener") %>%
-     addRasterImage(raster('data/0.5_1_0.5_0.5.tif'), colors = rainbow(10), opacity = 0.8,group="Richer") %>%
-     addRasterImage(raster('data/0.5_0.5_1_0.5.tif'), colors = rainbow(10), opacity = 0.8,group="Breathing") %>%
+     setView(lng = 5.66, lat = 51.96, zoom = 5) %>%
+     addRasterImage(raster('data/0.5_0.5_0.5_1.tif'), colors = col_pal, opacity = 0.6,group="Hazards Free") %>%
+     addRasterImage(raster('data/1_0.5_0.5_0.5.tif'), colors = col_pal, opacity = 0.6,group="Greener") %>%
+     addRasterImage(raster('data/0.5_1_0.5_0.5.tif'), colors = col_pal, opacity = 0.6,group="Richer") %>%
+     addRasterImage(raster('data/0.5_0.5_1_0.5.tif'), colors = col_pal, opacity = 0.6,group="Breathing") %>%
      addLayersControl(
        baseGroups = c("Greener", "Richer", "Breathing","Hazards Free"),
        options = layersControlOptions(collapsed = FALSE)
-    )
+    ) %>%
+     addLegend("bottomright", colors= col_pal, labels = c('1','2','3','4','5','6','7','8','9','10'),title="Living Quality Index") 
 
-saveWidget(mymap, file="mymap.html")
+saveWidget(mymap, file="mymap1.html")
 
