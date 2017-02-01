193a0042dd4f86c56edf248720544c62c466b3a1
Ubuntu User <ubuntu@packer-ubuntu-16.04-server-one>
2017-02-01 13:58:16 +0100

    some changes, all working

--- R/calc_index.R
+++ R/calc_index.R
@@ -1,6 +1,10 @@
 # calculate INDEX
 calc_index <- function(ndvi_mean,gecon,hazards,pm25,wei_ndvi_mean,wei_gecon,wei_hazards,wei_pm25){
-  wei_hazards <- -wei_hazards
-  wei_pm25 <- -wei_pm25
-  return (normalization(sum((ndvi_mean*wei_ndvi_mean),(gecon*wei_gecon),(pm25*wei_pm25),(hazards*wei_hazards),rm.na=T)))
+  a_ndvi <- calc(ndvi_mean, function(x) x*wei_ndvi_mean)
+  b_gecon <- calc(gecon, function(x) x*wei_gecon)
+  c_hazards <- calc(hazards, function(x) x*-wei_hazards)
+  d_pm25 <- calc(pm25, function(x) x*-wei_pm25)
+  i <- sum(a_ndvi,b_gecon,c_hazards,d_pm25)
+  i <- normalization(i)
+  return(i)
 }
