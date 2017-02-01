2b39377ea128ddfd1181a8f7b44ea8f27ac42c98
Ubuntu User <ubuntu@packer-ubuntu-16.04-server-one>
2017-02-01 15:59:39 +0100

    changes

--- R/calc_index.R
+++ R/calc_index.R
@@ -1,6 +1,6 @@
 # calculate INDEX
 calc_index <- function(ndvi_mean,gecon,hazards,pm25,wei_ndvi_mean,wei_gecon,wei_hazards,wei_pm25){
-  wei_hazards <- -wei_hazards
-  wei_pm25 <- -wei_pm25
-  return (normalization(sum((ndvi_mean*wei_ndvi_mean),(gecon*wei_gecon),(pm25*wei_pm25),(hazards*wei_hazards),rm.na=T)))
+#  wei_hazards <- -wei_hazards
+#  wei_pm25 <- -wei_pm25
+  return(normalization(sum((ndvi_mean*wei_ndvi_mean),(gecon*wei_gecon),(pm25*wei_pm25),(hazards*wei_hazards),rm.na=T)))
 }
